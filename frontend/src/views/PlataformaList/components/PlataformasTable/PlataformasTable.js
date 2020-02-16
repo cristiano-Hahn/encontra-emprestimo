9e539d989f4dcae2cf8142052db81057d19e4116
Paulo Cella <paulocell@unochapeco.edu.br>
2020-02-16 18:08:06 -0300

    oopa

--- frontend/src/views/PlataformaList/components/PlataformasTable/PlataformasTable.js
+++ frontend/src/views/PlataformaList/components/PlataformasTable/PlataformasTable.js
@@ -4,119 +4,93 @@ import PropTypes from 'prop-types';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import { makeStyles } from '@material-ui/styles';
 import {
-  Card,
-  CardActions,
-  CardContent,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TablePagination,
-  Link
+    Card,
+    CardContent,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow
 } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
-  root: {},
-  content: {
-    padding: 0
-  },
-  inner: {
-    minWidth: 1050
-  },
-  nameContainer: {
-    display: 'flex',
-    alignItems: 'center'
-  },
-  avatar: {
-    marginRight: theme.spacing(2)
-  },
-  actions: {
-    justifyContent: 'flex-end'
-  }
+    root: {},
+    content: {
+        padding: 0
+    },
+    inner: {
+        minWidth: 1050
+    },
+    nameContainer: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    avatar: {
+        marginRight: theme.spacing(2)
+    },
+    actions: {
+        justifyContent: 'flex-end'
+    }
 }));
 
 const PlataformasTable = props => {
-  const { className, plataformas, ...rest } = props;
+    const { className, plataformas, ...rest } = props;
 
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const [selectedplataformas] = useState([]);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [page, setPage] = useState(0);
+    const [selectedplataformas] = useState([]);
 
-
-  const handlePageChange = (event, page) => {
-    setPage(page);
-  };
-
-  const handleRowsPerPageChange = event => {
-    setRowsPerPage(event.target.value);
-  };
-
-  return (
-    <Card
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      <CardContent className={classes.content}>
-        <PerfectScrollbar>
-          <div className={classes.inner}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Avaliação geral</TableCell>
-                  <TableCell>Ações</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {plataformas.slice(0, rowsPerPage).map(plataforma => (
-                  <TableRow
-                    className={classes.tableRow}
-                    hover
-                    key={plataforma.id}
-                    selected={selectedplataformas.indexOf(plataforma.id) !== -1}
-                  >
-                    <TableCell>
-                      <div className={classes.nameContainer}>
-                        {plataforma.nome}
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <Rating readOnly value={plataforma.notaGeral} />
-                    </TableCell>
-                    <TableCell>
-                    <Link href={plataforma.endereco_online} >
-                      Ver detalhes
-                    </Link>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-        </PerfectScrollbar>
-      </CardContent>
-      <CardActions className={classes.actions}>
-        <TablePagination
-          component="div"
-          count={plataformas.length}
-          onChangePage={handlePageChange}
-          onChangeRowsPerPage={handleRowsPerPageChange}
-          page={page}
-          rowsPerPage={rowsPerPage}
-          rowsPerPageOptions={[5, 10, 25]}
-        />
-      </CardActions>
-    </Card>
-  );
+    return (
+        <Card
+            {...rest}
+            className={clsx(classes.root, className)}
+        >
+            <CardContent className={classes.content}>
+                <PerfectScrollbar>
+                    <div className={classes.inner}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>Avaliação geral</TableCell>
+                                    <TableCell>Ações</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {plataformas.map(plataforma => (
+                                    <TableRow
+                                        className={classes.tableRow}
+                                        hover
+                                        key={plataforma.id}
+                                        selected={selectedplataformas.indexOf(plataforma.id) !== -1}
+                                    >
+                                        <TableCell>
+                                            <div className={classes.nameContainer}>
+                                                {plataforma.nome}
+                                            </div>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Rating readOnly value={plataforma.notaGeral} />
+                                        </TableCell>
+                                        <TableCell>
+                                            <Link to={"/plataformas/" + plataforma.id+ "/detalhes"}  className="btn">Ver detalhes</Link>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </div>
+                </PerfectScrollbar>
+            </CardContent>
+        </Card>
+    );
 };
 
 PlataformasTable.propTypes = {
-  className: PropTypes.string,
-  plataformas: PropTypes.array.isRequired
+    className: PropTypes.string,
+    plataformas: PropTypes.array.isRequired
 };
 
 export default PlataformasTable;
